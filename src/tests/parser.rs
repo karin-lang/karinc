288a06d65beed507b54796bc22e0875fca1c381f
Garnet3106 <private@garnet.works>
2024-02-12 18:12:54 +0900

    Rearrange parser test cases

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -1,7 +1,9 @@
 #[cfg(test)]
+mod expr;
+#[cfg(test)]
 mod item;
 
-use crate::{ir::hir::*, lexer::token::*, parser::{Parser, ParserCombinatoryResult}};
+use crate::{ir::hir::*, lexer::token::*, parser::Parser};
 
 #[macro_export]
 macro_rules! number {
@@ -88,68 +90,4 @@ fn skips_unknown_syntax_token_with_error() {
     //         Vec::new(),
     //     ),
     // );
-}
-
-#[test]
-fn choices_any_expression() {
-    let tokens = vec![
-        (TokenPosition::default(), number!("0")),
-    ];
-
-    assert_eq!(
-        Parser::new().parse_expression(&mut tokens.iter().peekable()),
-        ParserCombinatoryResult::Matched(Some(HirExpression::Number(HirNumber("0".to_string())))),
-    );
-
-    let tokens = vec![
-        (TokenPosition::default(), id!("id")),
-    ];
-
-    assert_eq!(
-        Parser::new().parse_expression(&mut tokens.iter().peekable()),
-        ParserCombinatoryResult::Matched(Some(HirExpression::Identifier(HirIdentifier("id".to_string())))),
-    );
-}
-
-#[test]
-fn matches_number() {
-    let tokens = vec![
-        (TokenPosition::default(), number!("0")),
-    ];
-
-    assert_eq!(
-        Parser::new().parse_number(&mut tokens.iter().peekable()),
-        ParserCombinatoryResult::Matched(HirNumber("0".to_string())),
-    );
-}
-
-#[test]
-fn matches_identifier() {
-    let tokens = vec![
-        (TokenPosition::default(), id!("f")),
-    ];
-
-    assert_eq!(
-        Parser::new().parse_identifier(&mut tokens.iter().peekable()),
-        ParserCombinatoryResult::Matched(HirIdentifier("f".to_string())),
-    );
-}
-
-#[test]
-fn does_not_match_identifier() {
-    let tokens = vec![
-        (TokenPosition::default(), symbol!(OpenParen)),
-    ];
-
-    assert_eq!(
-        Parser::new().parse_identifier(&mut tokens.iter().peekable()),
-        ParserCombinatoryResult::Unmatched,
-    );
-
-    let tokens = Vec::new();
-
-    assert_eq!(
-        Parser::new().parse_identifier(&mut tokens.iter().peekable()),
-        ParserCombinatoryResult::Unmatched,
-    );
 }
---
+++ src/tests/parser/expr.rs
@@ -0,0 +1,65 @@
+use crate::{*, ir::hir::*, lexer::token::*, parser::{Parser, ParserCombinatoryResult}};
+
+#[test]
+fn choices_any_expression() {
+    let tokens = vec![
+        (TokenPosition::default(), number!("0")),
+    ];
+
+    assert_eq!(
+        Parser::new().parse_expression(&mut tokens.iter().peekable()),
+        ParserCombinatoryResult::Matched(Some(HirExpression::Number(HirNumber("0".to_string())))),
+    );
+
+    let tokens = vec![
+        (TokenPosition::default(), id!("id")),
+    ];
+
+    assert_eq!(
+        Parser::new().parse_expression(&mut tokens.iter().peekable()),
+        ParserCombinatoryResult::Matched(Some(HirExpression::Identifier(HirIdentifier("id".to_string())))),
+    );
+}
+
+#[test]
+fn matches_number() {
+    let tokens = vec![
+        (TokenPosition::default(), number!("0")),
+    ];
+
+    assert_eq!(
+        Parser::new().parse_number(&mut tokens.iter().peekable()),
+        ParserCombinatoryResult::Matched(HirNumber("0".to_string())),
+    );
+}
+
+#[test]
+fn matches_identifier() {
+    let tokens = vec![
+        (TokenPosition::default(), id!("f")),
+    ];
+
+    assert_eq!(
+        Parser::new().parse_identifier(&mut tokens.iter().peekable()),
+        ParserCombinatoryResult::Matched(HirIdentifier("f".to_string())),
+    );
+}
+
+#[test]
+fn does_not_match_identifier() {
+    let tokens = vec![
+        (TokenPosition::default(), symbol!(OpenParen)),
+    ];
+
+    assert_eq!(
+        Parser::new().parse_identifier(&mut tokens.iter().peekable()),
+        ParserCombinatoryResult::Unmatched,
+    );
+
+    let tokens = Vec::new();
+
+    assert_eq!(
+        Parser::new().parse_identifier(&mut tokens.iter().peekable()),
+        ParserCombinatoryResult::Unmatched,
+    );
+}
