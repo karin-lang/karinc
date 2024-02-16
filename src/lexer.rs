4616fe9bc6c3e65cea0be8b22afc6af0201705d5
Garnet3106 <private@garnet.works>
2024-02-16 16:27:21 +0900

    Fix comma token

--- src/lexer.rs
+++ src/lexer.rs
@@ -98,7 +98,7 @@ impl Lexer {
         };
 
         let symbol = match first_char {
-            ',' => (1, SymbolToken::Colon),
+            ',' => (1, SymbolToken::Comma),
             ':' => {
                 let colon_symbol = (1, SymbolToken::Colon);
 
